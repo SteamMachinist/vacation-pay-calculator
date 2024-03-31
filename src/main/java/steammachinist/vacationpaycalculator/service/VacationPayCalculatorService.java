3264484713cd14c7cb32ee2e32d54c99e071eef7
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-04-01 02:57:41 +0300

    implemented calculatePayForDates

--- src/main/java/steammachinist/vacationpaycalculator/service/VacationPayCalculatorService.java
+++ src/main/java/steammachinist/vacationpaycalculator/service/VacationPayCalculatorService.java
@@ -1,23 +1,37 @@
 package steammachinist.vacationpaycalculator.service;
 
+import de.focus_shift.jollyday.core.HolidayManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.stream.IntStream;
 
 @Service
+@RequiredArgsConstructor
 public class VacationPayCalculatorService {
     private final int AVERAGE_WORK_DAYS_IN_MONTH = 20;
+    private final HolidayManager holidayManager;
+
+    private BigDecimal calculateDaySalary(BigDecimal monthlySalary) {
+        BigDecimal salary = monthlySalary.setScale(2, RoundingMode.HALF_UP);
+        return salary.divide(BigDecimal.valueOf(AVERAGE_WORK_DAYS_IN_MONTH), RoundingMode.HALF_UP);
+    }
 
     public BigDecimal calculatePayForDaysNumber(BigDecimal monthlySalary, int daysNumber) {
-        BigDecimal salary = monthlySalary.setScale(2, RoundingMode.HALF_UP);
-        BigDecimal daySalary = salary.divide(BigDecimal.valueOf(AVERAGE_WORK_DAYS_IN_MONTH), RoundingMode.HALF_UP);
-        return daySalary.multiply(BigDecimal.valueOf(daysNumber));
+        return calculateDaySalary(monthlySalary).multiply(BigDecimal.valueOf(daysNumber));
     }
 
     public BigDecimal calculatePayForDates(BigDecimal monthlySalary, LocalDate startDate, LocalDate endDate) {
-        BigDecimal pay = monthlySalary.setScale(2, RoundingMode.HALF_UP);
-        return pay;
+        long businessDaysNumber = IntStream.rangeClosed(0, (int) (endDate.toEpochDay() - startDate.toEpochDay()))
+                .mapToObj(startDate::plusDays)
+                .filter(date ->
+                        !date.getDayOfWeek().equals(java.time.DayOfWeek.SATURDAY) &&
+                        !date.getDayOfWeek().equals(java.time.DayOfWeek.SUNDAY) &&
+                        !holidayManager.isHoliday(date))
+                .count();
+        return calculateDaySalary(monthlySalary).multiply(BigDecimal.valueOf(businessDaysNumber));
     }
 }
