214cc572a4f7c8caaa03660ee6eeeec3e8d01233
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2024-04-01 02:14:08 +0300

    implemented validation

--- src/main/java/steammachinist/vacationpaycalculator/controller/CustomControllerAdvice.java
+++ src/main/java/steammachinist/vacationpaycalculator/controller/CustomControllerAdvice.java
@@ -1,10 +1,13 @@
 package steammachinist.vacationpaycalculator.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.UnsatisfiedServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
 
 @ControllerAdvice
 public class CustomControllerAdvice {
@@ -18,5 +21,18 @@ public class CustomControllerAdvice {
     public ResponseEntity<String> handleMissingParams(MethodArgumentTypeMismatchException ex) {
         String errorMessage = ex.getName() + " parameter has wrong type or format";
         return ResponseEntity.badRequest().body(errorMessage);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException e) {
+        List<String> errorMessages = e.getConstraintViolations()
+                .stream()
+                .map(constraintViolation -> {
+                    String propertyPath = constraintViolation.getPropertyPath().toString();
+                    return propertyPath.substring(propertyPath.lastIndexOf('.') + 1) + " " + constraintViolation.getMessage();
+                })
+                .toList();
+
+        return ResponseEntity.badRequest().body(errorMessages);
     }
 }
--- src/main/java/steammachinist/vacationpaycalculator/controller/VacationPayCalculatorController.java
+++ src/main/java/steammachinist/vacationpaycalculator/controller/VacationPayCalculatorController.java
@@ -1,6 +1,8 @@
 package steammachinist.vacationpaycalculator.controller;
 
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,19 +13,20 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @RestController
+@Validated
 @RequestMapping(value = "/calculate")
 @RequiredArgsConstructor
 public class VacationPayCalculatorController {
     private final VacationPayCalculatorService calculatorService;
 
     @GetMapping(params = {"monthlySalary", "daysNumber"})
-    public BigDecimal calculatePayByDaysNumber(@RequestParam("monthlySalary") BigDecimal monthlySalary,
-                                               @RequestParam("daysNumber") int daysNumber) {
+    public BigDecimal calculatePayByDaysNumber(@RequestParam("monthlySalary") @Min(0) BigDecimal monthlySalary,
+                                               @RequestParam("daysNumber") @Min(0) int daysNumber) {
         return calculatorService.calculatePayForDaysNumber(monthlySalary, daysNumber);
     }
 
     @GetMapping(params = {"monthlySalary", "startDate", "endDate"})
-    public BigDecimal calculatePayByDates(@RequestParam("monthlySalary") BigDecimal monthlySalary,
+    public BigDecimal calculatePayByDates(@RequestParam("monthlySalary") @Min(0) BigDecimal monthlySalary,
                                           @RequestParam("startDate") LocalDate startDate,
                                           @RequestParam("endDate") LocalDate endDate) {
         return calculatorService.calculatePayForDates(monthlySalary, startDate, endDate);
